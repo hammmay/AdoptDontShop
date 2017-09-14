4c057346a9bb2b8ded7c28f1b4aa9befd3ed8d8a
Laura Hamilton <laurahamilton9@gmail.com>
2017-09-14 16:43:11 -0700

    Add constants class and update git ignore to protect key.

---
+++ app/src/main/java/com/epicodus/adoptdontshop/Constants.java
@@ -0,0 +1,8 @@
+package com.epicodus.adoptdontshop;
+
+public class Constants {
+    public static final String ADOPTDONTSHOP_KEY = BuildConfig.ADOPTDONTSHOP_KEY;
+    public static final String ADOPTDONTSHOP_SECRET = BuildConfig.ADOPTDONTSHOP_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
