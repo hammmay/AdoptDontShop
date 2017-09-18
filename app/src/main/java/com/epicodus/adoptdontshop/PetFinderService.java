b5c3afdda3c40bd3d60a98d275509c467ecaaae6
Laura Hamilton <laurahamilton9@gmail.com>
2017-09-18 16:00:22 -0700

    Built URL for API request and have a working response.

--- app/src/main/java/com/epicodus/adoptdontshop/PetFinderService.java
+++ app/src/main/java/com/epicodus/adoptdontshop/PetFinderService.java
@@ -1,31 +1,33 @@
 package com.epicodus.adoptdontshop;
 
+import android.util.Log;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class PetFinderService {
 
     public static void findFriends(String location, Callback callback) {
-        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.PETFINDER_CONSUMER_KEY, Constants.PETFINDER_CONSUMER_SECRET);
-
-//        consumer.setTokenWithSecret(Constants.PETFINDER_TOKEN, Constants.PETFINDER_TOKEN_SECRET);
 
         OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new SigningInterceptor(consumer))
                 .build();
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.PETFINDER_BASE_URL).newBuilder();
+        urlBuilder.addEncodedQueryParameter("key", Constants.PETFINDER_CONSUMER_KEY);
         urlBuilder.addQueryParameter(Constants.PETFINDER_LOCATION_QUERY_PARAMETER, location);
+        urlBuilder.addQueryParameter("output", "full");
+        urlBuilder.addQueryParameter("format", "json");
         String url = urlBuilder.build().toString();
 
         Request request = new Request.Builder()
                 .url(url)
                 .build();
+
+        Log.v("Therequest looks like: ", request + "");
 
         Call call = client.newCall(request);
         call.enqueue(callback);
