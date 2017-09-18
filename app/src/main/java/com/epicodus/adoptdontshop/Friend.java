b0d3f9768bedea905378d1aef35f09f242f098fe
Laura Hamilton <laurahamilton9@gmail.com>
2017-09-18 16:45:12 -0700

    Begun model build.

---
+++ app/src/main/java/com/epicodus/adoptdontshop/Friend.java
@@ -0,0 +1,60 @@
+package com.epicodus.adoptdontshop;
+
+public class Friend {
+    private String mAnimal;
+    private String mBreed;
+    private String mSize;
+    private Character mSex;
+    private String mAge;
+    private String mOffset;
+
+    public Restaurant(String name, String phone, String website,
+                      double rating, String imageUrl, ArrayList<String> address,
+                      double latitude, double longitude, ArrayList<String> categories) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mCategories = categories;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public ArrayList<String> getCategories() {
+        return mCategories;
+    }
+}
