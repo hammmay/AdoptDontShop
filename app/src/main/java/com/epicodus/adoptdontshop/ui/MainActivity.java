f535087458025607a7042fc6ece450a210f060eb
Laura Hamilton <laurahamilton9@gmail.com>
2017-09-26 10:09:48 -0700

    Create event listeners and instruct the app when to stop listening.

--- app/src/main/java/com/epicodus/adoptdontshop/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/adoptdontshop/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Typeface;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,23 +15,31 @@ import android.widget.TextView;
 
 import com.epicodus.adoptdontshop.Constants;
 import com.epicodus.adoptdontshop.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private DatabaseReference mSearchedLocationReference;
+    private ValueEventListener mSearchedLocationReferenceListener;
 
 //    private SharedPreferences mSharedPreferences;
 //    private SharedPreferences.Editor mEditor;
 
-    private DatabaseReference mSearchedLocationReference;
 
-    @Bind(R.id.locationEditText) EditText mLocationEditText;
-    @Bind(R.id.findFriendsButton) Button mFindFriendsButton;
-    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
-    @Bind(R.id.findMissionButton) Button mFindMissionButton;
+    @Bind(R.id.locationEditText)
+    EditText mLocationEditText;
+    @Bind(R.id.findFriendsButton)
+    Button mFindFriendsButton;
+    @Bind(R.id.appNameTextView)
+    TextView mAppNameTextView;
+    @Bind(R.id.findMissionButton)
+    Button mFindMissionButton;
 
 
     @Override
@@ -40,6 +49,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
+        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    String location = locationSnapshot.getValue().toString();
+                    Log.d("Locations updated", "location: " + location);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -56,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(v == mFindFriendsButton) {
+        if (v == mFindFriendsButton) {
 
             String location = mLocationEditText.getText().toString();
 
@@ -80,7 +107,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                startActivity(intent);
 //            }
 //        }
-        if(v == mFindMissionButton) {
+        if (v == mFindMissionButton) {
             Intent intent = new Intent(MainActivity.this, MissionActivity.class);
             startActivity(intent);
         }
@@ -90,6 +117,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSearchedLocationReference.push().setValue(location);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
+    }
 //    private void addToSharedPreferences(String location) {
 //        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
 //    }
