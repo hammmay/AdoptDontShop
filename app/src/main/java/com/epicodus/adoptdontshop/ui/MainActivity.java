0f4a5d34e28c368213a2d3669a16a839cee0d1f3
Laura Hamilton <laurahamilton9@gmail.com>
2017-09-27 16:35:46 -0700

    Add logout capabilities within an added overflow menu.

--- app/src/main/java/com/epicodus/adoptdontshop/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/adoptdontshop/ui/MainActivity.java
@@ -7,14 +7,16 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-
 import com.epicodus.adoptdontshop.Constants;
 import com.epicodus.adoptdontshop.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,44 +27,14 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-//    private SharedPreferences mSharedPreferences;
-//    private SharedPreferences.Editor mEditor;
-//
-//    private DatabaseReference mSearchedLocationReference;
-//
-//    private ValueEventListener mSearchedLocationReferenceListener;
 
     @Bind(R.id.findFriendsButton) Button mFindFriendsButton;
-//    @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
     @Bind(R.id.findMissionButton) Button mFindMissionButton;
     @Bind(R.id.savedFriendsButton) Button mSavedFriendsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-//        mSearchedLocationReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference()
-//                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-//
-//        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-//
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
-//                    String location = locationSnapshot.getValue().toString();
-//                    Log.d("Locations updated", "location: " + location);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//
-//        });
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -71,29 +43,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/pacifico.ttf");
         mAppNameTextView.setTypeface(pacificoFont);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mEditor = mSharedPreferences.edit();
-
         mFindFriendsButton.setOnClickListener(this);
         mFindMissionButton.setOnClickListener(this);
         mSavedFriendsButton.setOnClickListener(this);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
     public void onClick(View v) {
         if (v == mFindFriendsButton) {
-
-//            String location = mLocationEditText.getText().toString();
-//
-//            saveLocationToFirebase(location);
-
-//                if(!(location).equals("")) {
-//                    addToSharedPreferences(location);
-//                }
-
             Intent intent = new Intent(MainActivity.this, FriendsListActivity.class);
-//            intent.putExtra("location", location);
             startActivity(intent);
         }
 
@@ -116,17 +101,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
     }
-
-//    public void saveLocationToFirebase(String location) {
-//        mSearchedLocationReference.push().setValue(location);
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
-//    }
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
 }
